5aeaaeae66ed4b01200f332c7b8bf6acc8752878
Krists <kpudzens@gmail.com>
2018-10-11 16:02:11 +0300

    k

--- app/src/main/cpp/SoundSystem.cpp
+++ app/src/main/cpp/SoundSystem.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <android/log.h>
 #include <AndroidIO/SuperpoweredAndroidAudioIO.h>
+#include <AndroidIO/SuperpoweredUSBAudio.h>
 #include <SuperpoweredAdvancedAudioPlayer.h>
 #include <SuperpoweredSimple.h>
 #include <SuperpoweredCPU.h>
@@ -136,4 +137,63 @@ Java_com_soundwallcontroller_MainActivity_Cleanup (
     delete audioIO;
     delete player;
     free(floatBuffer);
+}
+
+
+// Beautifying the ugly Java-C++ bridge (JNI) with these macros.
+#define PID com_soundwallcontroller_SuperpoweredUSBAudio // Java package name and class name. Don't forget to update when you copy this code.
+#define MAKE_JNI_FUNCTION(r, n, p) extern "C" JNIEXPORT r JNICALL Java_ ## p ## _ ## n
+#define JNI(r, n, p) MAKE_JNI_FUNCTION(r, n, p)
+
+// This is called by the SuperpoweredUSBAudio Java object when a USB device is connected.
+JNI(jint, onConnect, PID) (
+        JNIEnv *env,
+        jobject __unused obj,
+        jint deviceID,
+        jint fd,
+        jbyteArray rawDescriptor
+) {
+    jbyte *rd = env->GetByteArrayElements(rawDescriptor, NULL);
+    int dataBytes = env->GetArrayLength(rawDescriptor);
+    int r = SuperpoweredUSBSystem::onConnect(deviceID, fd, (unsigned char *)rd, dataBytes);
+    env->ReleaseByteArrayElements(rawDescriptor, rd, JNI_ABORT);
+
+    // r is 0 if SuperpoweredUSBSystem can't do anything with the connected device.
+    // r & 2 is true if the device has MIDI. Start receiving events.
+    //if (r & 2) SuperpoweredUSBMIDI::startIO(deviceID, NULL, onMidiReceived);
+
+    // r & 1 is true if the device has audio. Start output.
+    if (r & 1) {
+        // allocate struct for sine wave oscillator
+//        sineWaveOutput *swo = (sineWaveOutput *)malloc(sizeof(sineWaveOutput));
+//        if (swo) {
+//            swo->mul = 0.0f;
+//            swo->step = 0;
+//            SuperpoweredCPU::setSustainedPerformanceMode(true);
+//
+//            // Our preferred settings: 44100 Hz, 16 bits, 0 input channels, 256 output channels,
+//            // low latency. Superpowered will set up the audio device as close as it can to these.
+//            SuperpoweredUSBAudio::easyIO (
+//                    deviceID,                    // deviceID
+//                    44100,                       // sampling rate
+//                    16,                          // bits per sample
+//                    0,                           // numInputChannels
+//                    256,                         // numOutputChannels
+//                    SuperpoweredUSBLatency_Low,  // latency
+//                    swo,                         // clientData
+//                    audioProcessing              // SuperpoweredUSBAudioProcessingCallback
+//            );
+//        }
+    }
+    return r;
+}
+
+// This is called by the SuperpoweredUSBAudio Java object when a USB device is disconnected.
+JNI(void, onDisconnect, PID) (
+        JNIEnv * __unused env,
+        jobject __unused obj,
+        jint deviceID
+) {
+    SuperpoweredUSBSystem::onDisconnect(deviceID);
+    SuperpoweredCPU::setSustainedPerformanceMode(false);
 }
